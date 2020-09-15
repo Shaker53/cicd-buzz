e3447e8988f78b49a047a2b9a533c6ffdf086102
Shaker53 <ilya.pashkevich@gmail.com>
2020-09-15 13:52:55 +0300

    heroku-auto

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -3,5 +3,5 @@ wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install @heroku-cli/plugin-container-registry
 docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
-heroku container:release web
-heroku ps:scale web=1
+heroku container:release -a web
+heroku ps:scale -a web=1
