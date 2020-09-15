5e09184fa3d41f19505473ab875830dc3b3439cc
Shaker53 <ilya.pashkevich@gmail.com>
2020-09-15 12:56:44 +0300

    docker-auto

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,5 +5,5 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker push $TRAVIS_REPO_SLUG
+docker build -f Dockerfile -t $MY_VAR:$TAG .
+docker push $MY_VAR
